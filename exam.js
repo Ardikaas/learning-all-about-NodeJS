b215d626b10dbde5f0ce13e22e021d8521bfa6be
Ardikaas <ardikaajisetiawan@gmail.com>
2023-07-14 21:24:10 +0700

    how to make simple module and run it in node file

---
+++ exam.js
@@ -0,0 +1,5 @@
+const umur = (umur) =>{
+  return `umur saya ${umur}`
+}
+
+module.exports = umur //exporting module (it make your function is useabel for another node file)
--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const display = (nama) =>{
   console.log(`hi nama saya ${nama}`)
 }
 
-console.log(display(nama))
+display(nama)
 
 //use "node [file name]" to run node file
+
+const umur = require('./exam') //call module from another node file
+console.log(umur(20))

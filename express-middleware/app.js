4b8f57b374e4566acf5e30875ee5575465311ffb
Ardikaas <ardikaajisetiawan@gmail.com>
2023-08-29 21:24:19 +0700

    Express Middleware

---
+++ express-middleware/app.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const expressLayouts =require('express-ejs-layouts')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(morgan('dev'));
+
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  console.log('Time: ', Date.now());
+  next();
+})
+
+app.get('/', (req, res) => {
+  const mahasiswa = [
+    {
+      nama: 'Ardikaas',
+      email: 'ardikaas@gmail.com'
+    },
+    {
+      nama: 'Dodi',
+      email: 'dodi@gmail.com'
+    },
+    {
+      nama: 'Ucup',
+      email: 'Ucup@gmail.com'
+    }
+  ]
+  res.render('index',{
+    layout: 'component/main-layout',
+    nama: 'Ardikaas',
+    title: 'Express web server | Home',
+    mahasiswa: mahasiswa,
+  });
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    layout: 'component/main-layout',
+    title: 'Express web server | About' });
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { 
+    layout: 'component/main-layout',
+    title: 'Express web server | Contact' });
+})
+
+app.get('/product/:id/', (req, res) => {
+  res.send(`Product id : ${req.params.id} <br>
+  Category : ${req.query.category}`)
+})
+
+app.get('/api', (req, res) => {
+  res.json({
+    nama: 'udin',
+    email: 'udin@gmail.com',
+    nohp: '01234123'
+  })
+})
+
+app.use('/', (req, res) => {
+  res.status(404)
+  res.send('<h1>404</h1>')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})

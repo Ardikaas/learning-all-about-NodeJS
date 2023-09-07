35dfeee89ff7b00a7b5e01982aaf58af61bf1946
Ardikaas <ardikaajisetiawan@gmail.com>
2023-09-07 12:24:00 +0700

    just made update and remove methode

--- rest-api-node-express-mongodb/app/controllers/mahasiswa.controller.js
+++ rest-api-node-express-mongodb/app/controllers/mahasiswa.controller.js
@@ -11,17 +11,40 @@ exports.create = (req,res) => {
 }
 
 exports.showAll = (req,res) => {
-  res.json({message: 'working'})
+  mahasiswa.find()
+    .then(data => res.send(data))
+    .catch(err => res.status(500).send({message: err.message}))
 }
 
 exports.show = (req,res) => {
-
+  const id = req.params.id;
+  mahasiswa.findById(id)
+    .then(data => res.send(data))
+    .catch(err => res.status(500).send({message: err.message}))
 }
 
 exports.update = (req,res) => {
+  const id = req.params.id;
 
+  req.body.tanggal_lahir = new Date(req.body.tanggal_lahir)
+  mahasiswa.findByIdAndUpdate(id, req.body, {useFindAndModify: false})
+    .then(data => {
+      if(!data){
+        res.status(404).send({message: "failed"})
+      }
+      res.send({message: "success"})
+    })
+    .catch(err => res.status(500).send({message: err.message}))
 }
 
 exports.delete = (req,res) => {
-
+  const id = req.params.id;
+  mahasiswa.findByIdAndRemove(id)
+    .then(data => {
+      if (!data){
+        res.status(404).send({message: "failed"})
+      }
+      res.send({message: "success"})
+    })
+    .catch(err => res.status(500).send({message: err.message}))
 }
